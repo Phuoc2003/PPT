cc2a1a326499efa3238f2e9472bee172c942e01a
LuckyBoi <21200333@student.hcmus.edu.vn>
2023-11-02 16:15:37 +0700

    Final script

--- Week3/Bai3.m
+++ Week3/Bai3.m
@@ -1,8 +1,8 @@
-t = 0:0.0001:0.02; 
+t = 0:0.00001:0.02; 
 v = 10 * cos(377 * t);
 i = 5 * cos(377 * t + 60);
 plot(t, v, 'b', t, i, 'r');
 xlabel('t');
-ylabel('value');
+ylabel('v(t), i(t)');
 legend('v(t)', 'i(t)');
 grid on;
