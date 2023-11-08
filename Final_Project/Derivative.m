fd6d6f90eef6badba58c7cc2796ab41b4b8b9b5e
LuckyBoi <21200333@student.hcmus.edu.vn>
2023-11-08 20:10:03 +0700

    Create Derivative class

---
+++ Final_Project/Derivative.m
@@ -0,0 +1,19 @@
+classdef Derivative < handle
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+
+    properties
+        xa;
+        ya;
+        f symfun;
+        h;
+        Oh;
+        derivedMethod;
+        x;
+        result;
+    end
+
+    methods
+       
+    end
+end
