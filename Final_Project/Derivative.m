1476f43155aab5a0b5f8279a67a83c4d2bf7a777
LuckyBoi <21200333@student.hcmus.edu.vn>
2023-11-20 20:35:08 +0700

    Added Derivative methods, DHCalculateValue callback. Testing all methods

--- Final_Project/Derivative.m
+++ Final_Project/Derivative.m
@@ -15,6 +15,35 @@ classdef Derivative < handle
     end
 
     methods
-       
-    end
+        function result = Lagrange(obj, xi)
+            n = length(obj.xa);
+            sum = 0;
+            for i = 1:n
+                product = obj.ya(i);
+                for j = 1:n
+                    if i ~= j
+                        product = (product * (xi-obj.xa(j)))/(obj.xa(i)-obj.xa(j));
+                    end
+                end
+                sum = sum + product;
+            end
+        result = sum;
+        end
+
+        function result = LagrangePolynominal(obj)
+            syms f(xi);
+            f(xi) = obj.Lagrange(xi);
+            f(xi) = simplify(f(xi));
+            result = f(xi);
+        end
+
+        function result = LagrangeDerivative(obj)
+            syms f(xi);
+            f(xi) = obj.LagrangePolynominal();
+            obj.fx = f(xi);
+            obj.h = obj.xa(2) - obj.xa(1);
+            obj.result = double((f(obj.x+obj.h)-f(obj.x))/obj.h);
+            result = obj.result;
+        end
+    end   
 end
