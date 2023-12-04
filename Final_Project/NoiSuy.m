39a814ce68ff92aabacd4fba70e7227baf6b577e
LuckyBoi <21200333@student.hcmus.edu.vn>
2023-12-04 20:23:26 +0700

    Modify all the methods of NoiSuy class

--- Final_Project/NoiSuy.m
+++ Final_Project/NoiSuy.m
@@ -7,45 +7,44 @@ classdef NoiSuy < handle
         ya;
         x;
         ppNoiSuy;
-        result;
     end
 
     methods
-        function [d] =  DividedDifference(xa, ya)
-            n = length(xa);
-            d = ya;
+        function d =  DividedDifference(obj)
+            n = length(obj.xa);
+            d = obj.ya;
             for i = 1:n
                 for j = 1:i-1
-                    d(i) = (d(i) - d(j)) / (xa(i) - xa(j));
+                    d(i) = (d(i) - d(j)) / (obj.xa(i) - obj.xa(j));
                 end
             end
         end
 
-        function result = NewtonForm(xa, da, x)
+        function result = NewtonForm(obj, da)
             n = length(da);
             result = da(n);
 
             for i = n-1:-1:1
-                result = result * (x - xa(i)) + da(i);
+                result = result * (obj.x - obj.xa(i)) + da(i);
             end
         end
 
-        function result = NewtonInterpolation(xa, ya, x)
-            da = DividedDifference(xa, ya);
-            result = NewtonForm(xa, da, x);
+        function result = NewtonInterpolation(obj)
+            da = obj.DividedDifference();
+            result = obj.NewtonForm(da);
         end
 
-        function expression = NewtonInterpolationExpression(xa, ya)
+        function expression = NewtonInterpolationExpression(obj)
 
-            n = length(xa);
-            da = DividedDifference(xa, ya);
+            n = length(obj.xa);
+            da = obj.DividedDifference();
             syms x;
 
-            result = ya(1); % Giá trị ban đầu
+            result = obj.ya(1); % Giá trị ban đầu
             for i = 2:n
                 tem = 1;
                 for j = 1:i - 1
-                    tem = tem * (x - xa(j));
+                    tem = tem * (obj.x - obj.xa(j));
                 end
                 result = result + da(i) * tem;
             end
@@ -54,39 +53,27 @@ classdef NoiSuy < handle
             fprintf('Biểu thức nội suy Newton: %s\n', char(expression));
         end
 
-        function result = Lagrange(xa, ya, x)
-            n = length(xa)
-            sum = 0
+        function result = Lagrange(obj, xi)
+            n = length(obj.xa);
+            sum = 0;
             for i = 1:n
-                product = ya(i)
-                for j= 1:n
-                    if (i ~= j)
-                        product= product*(x-xa(j)) / (xa(i)-xa(j))
-                    end
-                end
-                sum = sum + product
-            end
-            result = sum
-        end
-
-        function LagrangeInterpolation = ExpressionLagrangeInterpolation(xa, ya)
-            syms x;
-            n = length(xa);
-            LagrangeInterpolation = 0;
-
-            for i = 1:n
-                Li = 1;
+                product = obj.ya(i);
                 for j = 1:n
                     if i ~= j
-                        Li = Li * (x - xa(j)) / (xa(i) - xa(j));
+                        product = (product * (xi - obj.xa(j)))/(obj.xa(i) - obj.xa(j));
                     end
                 end
-                LagrangeInterpolation = LagrangeInterpolation + Li * ya(i);
+                sum = sum + product;
             end
-
-            disp('Đa thức nội suy Lagrange:');
-            disp(LagrangeInterpolation);
+            result = sum;
         end
-        
+
+         function result = LagrangePolynominal(obj)
+            syms f(xi);
+            f(xi) = obj.Lagrange(xi);
+            f(xi) = simplify(f(xi));
+            result = f(xi);
+        end
+
     end
 end
