e32ca39b3c7cad15ae2f652a4f7028c87b54b3ed
LuckyBoi <21200333@student.hcmus.edu.vn>
2023-12-05 19:17:00 +0700

    Revert "Change NoiSuy methods to improve performance"
    This reverts commit b64b82920e3b689a65c3e44e3a2153abcd9c8d0b.

--- Final_Project/NoiSuy.m
+++ Final_Project/NoiSuy.m
@@ -20,25 +20,37 @@ classdef NoiSuy < handle
             end
         end
 
-        function result = NewtonForm(obj, da, xi)
+        function result = NewtonForm(obj, da)
             n = length(da);
             result = da(n);
 
             for i = n-1:-1:1
-                result = result * (xi - obj.xa(i)) + da(i);
+                result = result * (obj.x - obj.xa(i)) + da(i);
             end
         end
 
-        function result = NewtonInterpolation(obj, xi)
+        function result = NewtonInterpolation(obj)
             da = obj.DividedDifference();
-            result = obj.NewtonForm(da, xi);
+            result = obj.NewtonForm(da);
         end
 
-        function result = NewtonPolynominal(obj)
-            syms f(xi);
-            f(xi) = obj.NewtonInterpolation(xi);
-            f(xi) = simplify(f(xi));
-            result = f(xi);
+        function expression = NewtonInterpolationExpression(obj)
+
+            n = length(obj.xa);
+            da = obj.DividedDifference();
+            syms x;
+
+            result = obj.ya(1); % Giá trị ban đầu
+            for i = 2:n
+                tem = 1;
+                for j = 1:i - 1
+                    tem = tem * (obj.x - obj.xa(j));
+                end
+                result = result + da(i) * tem;
+            end
+
+            expression = result;
+            fprintf('Biểu thức nội suy Newton: %s\n', char(expression));
         end
 
         function result = Lagrange(obj, xi)
